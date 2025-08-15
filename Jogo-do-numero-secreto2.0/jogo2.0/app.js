3482c5ce4e629d49e56c96f2f8844a9a21cae08d
Matheus Antero <101403401+Anterosuehtam@users.noreply.github.com>
2025-08-15 10:43:31 -0300

    Alterando limite para 100

--- Jogo-do-numero-secreto2.0/jogo2.0/app.js
+++ Jogo-do-numero-secreto2.0/jogo2.0/app.js
@@ -1,5 +1,5 @@
 listaDeNumerosSorteados = [];
-let limiteNumeros = 20;
+let limiteNumeros = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 20');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 exibirMensagemInicial();
 
